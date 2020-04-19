cdf35a6c70fd904ea0d4870980a7b43cda7b22e2
unknown <magvin@gihub.com>
2020-04-19 03:05:11 -0700

    GO-2 Init module

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/magvin/webservice/models"
 )
 
 func main() {
-	fmt.Println("Hello from a module, Gophers!")
+	u := models.User{
+		ID:        2,
+		FirstName: "Tricia",
+		LastName:  "McMillan",
+	}
+	fmt.Println(u)
 }
